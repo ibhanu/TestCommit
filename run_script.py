b6422b227adc84a5ac422871b6f0aa886135f243
Bhanu Pratap <pratap.bhanu3434@gmail.com>
2020-06-05 19:19:02 +0000

    File Changed at Fri Jun  5 19:19:02 UTC 2020

--- run_script.py
+++ run_script.py
@@ -9,7 +9,9 @@ logging.basicConfig(filename="script.log",
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.info("Logger Started")
+from pushsafer import init, Client
 
+init("O96xi4uzfIGkzMG3oeDb")
 
 def script():
     os.system("sh commit.sh")
@@ -19,6 +21,8 @@ def run_shell():
     try:
         logger.debug("Commit Started")
         script()
+        Client("").send_message("Message", "Hello", "25630", "1", "4", "2", "https://www.pushsafer.com", "Open Pushsafer", "0", "2", "60", "600", "1", "", "", "")
+
         logger.debug("Commit ended")
     except:
         logger.exception("Exception Occurred")
